0f86b54bd872c50907eadb3265a62bab2eab0d91
jntakpe <j.ntakpe@gmail.com>
2016-12-05 23:45:16 +0100

    Ajout d'une exception indiquant l'absence d'un client

--- reactive-app/src/main/java/com/github/jntakpe/reactiveapp/exceptions/ClientNotFoundException.java
+++ reactive-app/src/main/java/com/github/jntakpe/reactiveapp/exceptions/ClientNotFoundException.java
@@ -1,8 +1,14 @@
 package com.github.jntakpe.reactiveapp.exceptions;
 
 /**
+ * Exception indiquant que le client demandé n'existe pas
+ *
  * @author jntakpe
  */
-public class NoClientException {
+public class ClientNotFoundException extends RuntimeException {
+
+    public ClientNotFoundException(String message) {
+        super(message);
+    }
 
 }
